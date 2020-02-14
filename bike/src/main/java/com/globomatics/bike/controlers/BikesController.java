ddd0abdedf103dd3983c53b5091e47f1873d3d50
Vibesx <lucian.paslariu@gmail.com>
2020-02-14 20:56:36 +0200

    Created repo; Model improvements;

--- bike/src/main/java/com/globomatics/bike/controlers/BikesController.java
+++ bike/src/main/java/com/globomatics/bike/controlers/BikesController.java
@@ -1,6 +1,8 @@
 package com.globomatics.bike.controlers;
 
 import com.globomatics.bike.models.Bike;
+import com.globomatics.bike.repositories.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,20 +19,22 @@ import java.util.List;
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
 
+    @Autowired
+    BikeRepository bikeRepository;
+
     @GetMapping
     public List<Bike> list() {
-        List<Bike> bikes = new ArrayList<>();
-        return bikes;
+        return bikeRepository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Bike bike) {
-
+        bikeRepository.save(bike);
     }
 
     @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id) {
-        return new Bike();
+        return bikeRepository.getOne(id);
     }
 }
